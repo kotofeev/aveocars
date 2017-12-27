55dcc7b29f92c6faf4e8bc1b48cc176be3a31e35
Seo <privatecottage@yandex.ru>
2017-12-27 18:13:55 +0300

    Внесение правок в подвал

--- inc/bottom.inc.php
+++ inc/bottom.inc.php
@@ -2,3 +2,6 @@
 drawMenu($leftMenu, false);
 ?>
 &copy; Супер Мега Веб-мастер, 2000 &ndash; 2015
+    <p>
+            Powered by <?=$_SERVER["SERVER_SOFTWARE"]?> /PHP<?=PHP_VERSION?> on <?=PHP_OS?>
+    </p>
